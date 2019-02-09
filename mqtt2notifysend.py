5e587d6bfdfd10b83cc3253d6c3f1a9c6ff50630
David Lorenzo <17401854+Pythoneiro@users.noreply.github.com>
2019-02-09 23:22:53 +0100

    Initial upload

---
+++ mqtt2notifysend.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import paho.mqtt.client as mqtt
+
+BROKER = "192.168.0.90"
+TOPIC_SUB = "dev/pc/toast"
+TOPIC_STAT = "dev/pc/stat"
+PORT = 1883
+KEEPALIVE = 60
+PAYLOAD_ON = "Client Online"
+PAYLOAD_OFF = "Client Offline"
+DELIMITER = ";;;"
+DEFAULT_TITLE = "MQTT"
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+ICON = os.path.join(CURRENT_DIR, "icon.png")
+
+
+def on_connect(client: mqtt.Client, userdata, flags, rc):
+    client.subscribe(TOPIC_SUB)
+    client.publish(TOPIC_STAT, PAYLOAD_ON, retain=True)
+
+
+def on_message(client: mqtt.Client, userdata, msg: mqtt.MQTTMessage):
+    payload = msg.payload.decode()
+    if DELIMITER in payload:
+        title, text = payload.split(DELIMITER)
+    else:
+        title = DEFAULT_TITLE
+        text = payload
+    subprocess.call("notify-send -i '{icon}' '{title}' '{text}'".format(
+        icon=ICON,
+        title=title,
+        text=text
+    ), shell=True)
+
+
+mqttClient = mqtt.Client()
+mqttClient.on_connect = on_connect
+mqttClient.on_message = on_message
+mqttClient.will_set(TOPIC_STAT, PAYLOAD_OFF, retain=True)
+mqttClient.connect(BROKER, PORT, KEEPALIVE)
+
+if __name__ == "__main__":
+    try:
+        mqttClient.loop_forever()
+    except KeyboardInterrupt:
+        mqttClient.loop_stop()
