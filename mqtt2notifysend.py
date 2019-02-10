152e7d1e9368cf106d0514c71eb5e64abd65b4be
Roberto Lodeiro <34032826+Lodeiro0001@users.noreply.github.com>
2019-02-10 19:06:36 +0100

    Add SSL/TLS, default to None

--- mqtt2notifysend.py
+++ mqtt2notifysend.py
@@ -4,13 +4,23 @@ import os
 import subprocess
 import paho.mqtt.client as mqtt
 
-BROKER = "192.168.0.90"
-TOPIC_SUB = "dev/pc/toast"
-TOPIC_STAT = "dev/pc/stat"
-PORT = 1883
+# The CLIENT_NAME must be unique on the broker.
+# Set your username, your password and change the port to 8883
+# if you use an authenticated SSL/TLS connection with the broker.
+
+BROKER = "192.168.X.XXX"
+CLIENT_NAME = "your_computer_name"
+USERNAME = None
+PASSWORD = None
+CLEAN_SESSION = True
+USER_DATA = None
+PROTOCOL = mqtt.MQTTv311
+TOPIC_SUB = "dev/"+CLIENT_NAME+"/toast"
+TOPIC_STAT = "dev/"+CLIENT_NAME+"/stat"
+PORT = 8883
 KEEPALIVE = 60
-PAYLOAD_ON = "Client Online"
-PAYLOAD_OFF = "Client Offline"
+PAYLOAD_ON = "Online"
+PAYLOAD_OFF = "Offline"
 DELIMITER = ";;;"
 DEFAULT_TITLE = "MQTT"
 
@@ -18,31 +28,27 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 ICON = os.path.join(CURRENT_DIR, "icon.png")
 
 
-def on_connect(client: mqtt.Client, userdata, flags, rc):
-    client.subscribe(TOPIC_SUB)
-    client.publish(TOPIC_STAT, PAYLOAD_ON, retain=True)
+def on_connect(mqtt_client: mqtt.Client, userdata, flags, rc):
+    mqtt_client.subscribe(TOPIC_SUB)
+    mqtt_client.publish(TOPIC_STAT, PAYLOAD_ON, retain=True)
+    mqttClient.will_set(TOPIC_STAT, PAYLOAD_OFF, retain=True)
+    if USERNAME is not None and PASSWORD is not None:
+        mqttClient.username_pw_set(USERNAME, PASSWORD)
 
 
-def on_message(client: mqtt.Client, userdata, msg: mqtt.MQTTMessage):
+def on_message(mqtt_client: mqtt.Client, userdata, msg: mqtt.MQTTMessage):
     payload = msg.payload.decode()
     if DELIMITER in payload:
         title, text = payload.split(DELIMITER)
     else:
         title = DEFAULT_TITLE
         text = payload
-    subprocess.call((
-        "notify-send",
-        "-i",
-        ICON,
-        title,
-        text
-    ), shell=False)
+    subprocess.call(("notify-send", "-i", ICON, title, text))
 
 
-mqttClient = mqtt.Client()
+mqttClient = mqtt.Client(CLIENT_NAME, CLEAN_SESSION, USER_DATA, PROTOCOL)
 mqttClient.on_connect = on_connect
 mqttClient.on_message = on_message
-mqttClient.will_set(TOPIC_STAT, PAYLOAD_OFF, retain=True)
 mqttClient.connect(BROKER, PORT, KEEPALIVE)
 
 if __name__ == "__main__":
