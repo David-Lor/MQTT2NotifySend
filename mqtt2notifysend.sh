ee1835aabccc6dfd62c3c4572c856e35d75cdfb6
David-Lor <17401854+David-Lor@users.noreply.github.com>
2021-11-20 15:52:50 +0100

    feat: support sending icons base64-encoded as strings

--- mqtt2notifysend.sh
+++ mqtt2notifysend.sh
@@ -5,7 +5,8 @@
 # {"title": "Notification title", "text": "Notification body", "level": "Notification urgency"}
 #   - text: only required key
 #   - title: if not set, using DEFAULT_TITLE
-#   - level: notify-send urgency, one of: low, normal, critical
+#   - level: notify-send urgency, one of: low, normal, critical; if not set, using DEFAULT_LEVEL
+#   - iconB64: notify-send icon (image file encoded as base64 string)
 
 if [[ $1 == "--help" ]]
 then
@@ -17,51 +18,78 @@ RECONNECTION_DELAY=${RECONNECTION_DELAY:=5}
 DEFAULT_TITLE=${DEFAULT_TITLE:="MQTT2NotifySend"}
 DEFAULT_LEVEL=${DEFAULT_LEVEL:="normal"}
 LOG_ENABLE=${LOG_ENABLE:="false"}
+ICON_BASE_PATH=${ICON_BASE_PATH:="/tmp/mqtt2notifysend-icon"}
+ICON_DELETE_DELAY=${ICON_DELETE_DELAY:=5}
 
-log_counter=0
 
 function log() {
     if [[ "${LOG_ENABLE}" == "true" || "${LOG_ENABLE}" == "1" ]]
     then
-        echo "[#$log_counter] $1"
+        echo "[#$loop_counter] $1"
     fi
 }
 
-while true
-do
-    # TODO Identify when mosquitto_sub fails due to connection error or user input (args) error
-    mosquitto_sub "$@" | while read -r payload
+function getRandomID() {
+    echo $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c10)
+}
+
+function loadIconFromB64() {
+    log "Decoding base64 icon..."
+    b64data="$1"
+    icon_path="$ICON_BASE_PATH-$session_id-$(getRandomID)"
+    echo "$b64data" | base64 --decode - > "$icon_path"
+    (sleep $ICON_DELETE_DELAY && rm "$icon_path") &
+}
+
+function mqtt2notifysendMain() {
+    session_id=$(getRandomID)
+    loop_counter=0
+
+    while true
     do
-        log_counter=$(( log_counter + 1))
+        # TODO Identify when mosquitto_sub fails due to connection error or user input (args) error
+        mosquitto_sub "$@" | while read -r payload
+        do
+            loop_counter=$(( loop_counter + 1))
 
-        log "Received message with payload=\"${payload}\""
-        title=$(echo "${payload}" | jq -r ".title")
-        text=$(echo "${payload}" | jq -r ".text")
-        level=$(echo "${payload}" | jq -r ".level")
+            log "Received message with payload=\"${payload}\""
+            title=$(echo "${payload}" | jq -r ".title")
+            text=$(echo "${payload}" | jq -r ".text")
+            level=$(echo "${payload}" | jq -r ".level")
+            icon_b64=$(echo "${payload}" | jq -r ".iconB64")
 
-        if [[ "${text}" == "null" || -z "${text}" ]]
-        then
-            log "The payload was empty - notification will not be send"
-            continue
-        fi
+            if [[ "${text}" == "null" || -z "${text}" ]]
+            then
+                log "The payload was empty - notification will not be send"
+                continue
+            fi
 
-        if [[ "${title}" == "null" || -z "${title}" ]]
-        then
-            log "Title not set - using default"
-            title="${DEFAULT_TITLE}"
-        fi
+            if [[ "${title}" == "null" || -z "${title}" ]]
+            then
+                log "Title not set - using default"
+                title="${DEFAULT_TITLE}"
+            fi
 
-        if [[ "${level}" == "null" || -z "${level}" ]]
-        then
-            log "Level (urgency) not set - using default"
-            level="${DEFAULT_LEVEL}"
-        fi
+            if [[ "${level}" == "null" || -z "${level}" ]]
+            then
+                log "Level (urgency) not set - using default"
+                level="${DEFAULT_LEVEL}"
+            fi
 
-        log "Sending notification with title=\"${title}\" - text=\"${text}\""
-        set -x
-        notify-send --urgency=${level} "${title}" "${text}"
-        set +x
+            icon_path=""
+            if [[ "${icon_b64}" != "null" && ! -z "${icon_b64}" ]]
+            then
+                loadIconFromB64 "${icon_b64}"
+            fi
+
+            log "Sending notification with title=\"${title}\" - text=\"${text}\""
+            set -x
+            notify-send --urgency=${level} --icon="${icon_path}" "${title}" "${text}"
+            set +x
+        done
+
+        sleep $RECONNECTION_DELAY
     done
+}
 
-    sleep $RECONNECTION_DELAY
-done
+mqtt2notifysendMain "$@"
